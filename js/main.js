83b9185de56350ce5740c766c57ab6de2e404255
Marco Giammari <marcogiammari@gmail.com>
2023-05-08 15:46:20 +0200

    add(vue) change caption method

--- js/main.js
+++ js/main.js
@@ -9,19 +9,22 @@ createApp({
                 source: 'https://picsum.photos/200',
                 altTxt: "Immagine random presa da Picsum"
             },
-            bgColor: ""
+            bgColor: "",
+            inputCaption: ""
         }
     },
     methods: {
-        changeTitle() {
-            console.log("funziona");
-        },
-        changeColor() {
+        // metodo (con l'orribile style inline) per settare uno sfondo di colore casuale
+        randomBackgroundColor() {
             x = Math.floor(Math.random() * 256);
             y = Math.floor(Math.random() * 256);
             z = Math.floor(Math.random() * 256);
             this.bgColor = `background-color: rgb(${x},${y},${z}`;
             console.log(this.bgColor);
+        },
+        // metodo per cambiare la caption dell'immagine
+        changeCaption() {
+            this.image.altTxt = this.inputCaption;
         }
     }
 }).mount('#app')
