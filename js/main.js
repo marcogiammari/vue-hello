2e9d1a8e61a6495f26a3933da2871a97e97d5781
Marco Giammari <marcogiammari@gmail.com>
2023-05-08 15:34:07 +0200

    add(vue) change bg-color

--- js/main.js
+++ js/main.js
@@ -8,8 +8,20 @@ createApp({
             image: {
                 source: 'https://picsum.photos/200',
                 altTxt: "Immagine random presa da Picsum"
+            },
+            bgColor: ""
         }
-
+    },
+    methods: {
+        changeTitle() {
+            console.log("funziona");
+        },
+        changeColor() {
+            x = Math.floor(Math.random() * 256);
+            y = Math.floor(Math.random() * 256);
+            z = Math.floor(Math.random() * 256);
+            this.bgColor = `background-color: rgb(${x},${y},${z}`;
+            console.log(this.bgColor);
         }
     }
 }).mount('#app')
